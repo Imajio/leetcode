a24cc83a4650896dbe4620642d543fc6ba7dc912
Imajio <inkognito.gomel@gmail.com>
2024-05-21 23:10:56 +0200

    4. Median of Two Sorted Arrays

---
+++ solutions/src/hard/median_of_two_sorted_arrays/Solution.java
@@ -0,0 +1,39 @@
+package hard.median_of_two_sorted_arrays;
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums1 = new int[2];
+        nums1[0] = 1;
+        nums1[1] = 2;
+        int[] nums2 = new int[2];
+        nums2[0] = 3;
+        nums2[1] = 4;
+        System.out.println(findMedianSortedArrays(nums1,nums2));
+    }
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int i = 0, j = 0, m1 = 0, m2 = 0;
+
+        for (int count = 0; count <= (n + m) / 2; count++) {
+            m2 = m1;
+            if (i != n && j != m) {
+                if (nums1[i] > nums2[j]) {
+                    m1 = nums2[j++];
+                } else {
+                    m1 = nums1[i++];
+                }
+            } else if (i < n) {
+                m1 = nums1[i++];
+            } else {
+                m1 = nums2[j++];
+            }
+        }
+
+        if ((n + m) % 2 == 1) {
+            return (double) m1;
+        } else {
+            double ans = (double) m1 + (double) m2;
+            return ans / 2.0;
+        }
+    }
+}
