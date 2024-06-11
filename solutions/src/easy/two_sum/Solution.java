1389f639953f08064592e875c778decb4f022f64
Imajio <inkognito.gomel@gmail.com>
2024-06-11 21:45:10 +0200

    1. Two Sum

---
+++ solutions/src/easy/two_sum/Solution.java
@@ -0,0 +1,22 @@
+package easy.two_sum;
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> prevMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (prevMap.containsKey(diff)) {
+                return new int[] { prevMap.get(diff), i };
+            }
+
+            prevMap.put(num, i);
+        }
+
+        return null;
+    }
+}
