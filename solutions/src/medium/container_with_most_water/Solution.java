919a919b5505bb69bb1e2e319acd74edf12260ea
D <=>
2025-11-03 12:08:10 +0100

    11. Container With Most Water

---
+++ solutions/src/medium/container_with_most_water/Solution.java
@@ -0,0 +1,20 @@
+package medium.container_with_most_water;
+
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1;
+        int maxArea = 0;
+
+        while (l < r) {
+            int area = (r-l) * Math.min(height[l], height[r]);
+            maxArea = Math.max(maxArea, area);
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        
+        return maxArea;
+    }
+}
